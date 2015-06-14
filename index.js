404804deb39ec9583335525f355b2ba49a922624
Theo Pack <tf.pack@gmail.com>
2015-06-14 21:36:43 +0200

    Calc Auth Header

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var Hapi = require('hapi');
+var Base64 = require('./base64.js').Base64;
 
 // Create a server with a host and port
 var server = new Hapi.Server();
@@ -19,9 +20,10 @@ server.route({
     method: 'POST',
     path:'/config', 
     handler: function (request, reply) {
+        var authHeader = Base64.encode(request.payload.user + ':' + request.payload.token);
        reply({
         url: request.payload.url, 
-        token: request.payload.token
+        auth: authHeader
        });
     }
 });
